9d567e3cec74d2207f5e58f6b94dfcc82eb206f4
barakuda2008 <74827332+barakuda2008@users.noreply.github.com>
2020-11-21 17:42:52 +0200

    Update targil-1.js

--- targil-1.js
+++ targil-1.js
@@ -9,3 +9,19 @@ if ((num % 7 === 0) || (tens === 7) || (ones === 7)) {
 }else{
     console.log (num)
 }
+
+
+
+
+// targil 11
+var speed = parseInt (prompt ("enrer speed: "));
+//if ((speed < 120)||(speed === 120)) {
+if (speed <= 120) {
+    console.log ("you drive carefully")
+} else {
+    if ((speed > 120)&&(speed < 161)){
+    console.warn ("warning you drive too fast")
+    }else{
+        console.error ("you got a fine")
+    }    
+}
